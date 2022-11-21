b97926e5146e7454d2200d16ec1a95ca21f37bad
Aleksandr Tochilin <alex8079@gmail.com>
2022-11-21 16:41:07 +0300

    final version

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-let images = [{
+const images = [{
 	url: './images/slider-image1.jpg'
 }, {
-	url: './images/slider-image2.jpg'	
+	url: './images/slider-image2.jpg'
 }, {
-	url: './images/slider-image3.jpg'	
+	url: './images/slider-image3.jpg'
 }
 ];
 
@@ -13,10 +13,10 @@ function initSlider() {
 		return;
 	}
 
-	let sliderImages = document.querySelector(".slider-images");
-	let sliderControl = document.querySelector(".slider-control");
-	let sliderButtons = document.querySelector(".slider-buttons");
-
+	const sliderImages = document.querySelector(".slider-images");
+	const sliderControl = document.querySelector(".slider-control");
+	const sliderButtons = document.querySelector(".slider-buttons");
+	const projectInfo = document.querySelector(".completed-info");
 
 	initImages();
 	initArrows();
@@ -30,8 +30,10 @@ function initSlider() {
 		});
 	}
 
+	/* below there are a little bit complicated combinations of selectors because i did not want to add/remove classes from "design studio project part of which this slider is */
+
 	function initArrows() {
-		sliderControl.querySelectorAll(".slider-arrow").forEach(arrow => {
+		sliderControl.querySelectorAll(".nav-button:first-child, .nav-button:last-child").forEach(arrow => {
 			arrow.addEventListener("click", () => {
 				let currNumber = +sliderImages.querySelector(".active").dataset.index;
 				let nextNumber;
@@ -46,14 +48,67 @@ function initSlider() {
 	}
 
 	function initDots() {
-		images.forEach((image, index) => {
-			if (index === 0) {
-				sliderControl.querySelector(".slider-dot.n0").classList.add("active");
-			}
-		})
+		sliderControl.querySelectorAll(".nav-button:nth-child(2), .nav-button:nth-child(3),	.nav-button:nth-child(4)").forEach((dot, index) => {
+			dot.classList.add(`n${index}`);
 
-		sliderControl.querySelectorAll(".slider-dot").forEach((dot, index) => {
+			if (index === +sliderImages.querySelector(".active").dataset.index) {
+				dot.classList.add("active");
+			}
+	
 			dot.addEventListener("click", () => {
+				moveSlider(index);
+			})
+		})
+	}
+
+	function changeInfo(index) {
+		//array of objects with project's info
+		let completedProjects = [{
+			"city": "Rostov-on-Don<br>LCD admiral",
+			"time": "3.5 months",
+			"area": "81 m2"
+		}, {
+			"city": "Sochi<br>Thieves",
+			"time": "4 months",
+			"area": "105 m2"
+		}, {
+			"city": "Rostov-on-Don<br>Patriotic",
+			"time": "3 months",
+			"area": "93 m2"
+		}]
+
+		switch (index) {
+			case 0:
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(2)").innerHTML = completedProjects[index].city;
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(4)").textContent = completedProjects[index].time;
+				projectInfo.querySelector("dl:last-child .list-details:nth-child(2)").textContent = completedProjects[index].area;
+				break;
+		}
+		switch (index) {
+			case 1:
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(2)").innerHTML = completedProjects[index].city;
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(4)").textContent = completedProjects[index].time;
+				projectInfo.querySelector("dl:last-child .list-details:nth-child(2)").textContent = completedProjects[index].area;
+				break;
+		}
+		switch (index) {
+			case 2:
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(2)").innerHTML = completedProjects[index].city;
+				projectInfo.querySelector("dl:first-child .list-details:nth-child(4)").textContent = completedProjects[index].time;;
+				projectInfo.querySelector("dl:last-child .list-details:nth-child(2)").textContent = completedProjects[index].area;
+				break;
+		}
+	};
+
+	function initButtons() {
+		sliderButtons.querySelectorAll(".slider-button").forEach((btn, index) => {
+			btn.classList.add(`n${index}`)
+			
+			if (index === +sliderImages.querySelector(".active").dataset.index) {
+				btn.classList.add("active");
+			}
+
+			btn.addEventListener("click", () => {
 				moveSlider(index);
 			})
 		})
@@ -62,24 +117,14 @@ function initSlider() {
 	function moveSlider(num) {
 		sliderImages.querySelector(".active").classList.remove("active");
 		sliderImages.querySelector(`.n${num}`).classList.add("active");
-		sliderControl.querySelector(".slider-dot.active").classList.remove("active");
-		sliderControl.querySelector(`.slider-dot.n${num}`).classList.add("active");
+		sliderControl.querySelector(".nav-button.active").classList.remove("active");
+		sliderControl.querySelector(`.nav-button.n${num}`).classList.add("active");
 		sliderButtons.querySelector(".slider-button.active").classList.remove("active");
 		sliderButtons.querySelector(`.slider-button.n${num}`).classList.add("active");
+		//function changes completed projects on the left side
+		changeInfo(num);
 	}
 
-	function initButtons() {
-		images.forEach((image, index) => {
-			if (index === 0) {
-				sliderButtons.querySelector(".slider-button.n0").classList.add("active");
-			}
-		})
-		sliderButtons.querySelectorAll(".slider-button").forEach((btn, index) => {
-			btn.addEventListener("click", () => {
-				moveSlider(index);
-			})
-		})
-	}
 }
 
 document.addEventListener("DOMContentLoaded", () => {
